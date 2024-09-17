8c4d60fce406a06db5398aa1a938af38b4eca84a
Rafey Ahmad <rafeyahmad@protonmail.com>
2024-09-18 04:18:08 +0530

    Fix test

--- tests/unit/routes/footballers-test.js
+++ tests/unit/routes/footballers-test.js
@@ -1,11 +1,11 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-quickstart/tests/helpers';
 
-module('Unit | Route | people', function (hooks) {
+module('Unit | Route | footballers', function (hooks) {
   setupTest(hooks);
 
   test('it exists', function (assert) {
-    let route = this.owner.lookup('route:people');
+    let route = this.owner.lookup('route:footballers');
     assert.ok(route);
   });
 });
